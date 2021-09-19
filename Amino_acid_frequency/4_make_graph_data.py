accfadaeaab1a568e6d63ad67b08ff29334a4a23
kentnaoki <72868534+kentnaoki@users.noreply.github.com>
2021-09-19 16:21:39 +0100

    Add files via upload

--- Amino_acid_frequency/4_make_graph_data.py
+++ Amino_acid_frequency/4_make_graph_data.py
@@ -1,25 +1,22 @@
 #%%
 import pandas as pd
 import glob
+import re
+import argparse
+import matplotlib.pyplot as plt
 
-protein_names = ['N', 'NS9c', 'NSP12', 'NSP2'] #include clades that have differences in the most frequent amino acids
-clade_names = ['G', 'GH', 'GR', 'GV', 'L', 'O', 'S', 'V'] 
+parser = argparse.ArgumentParser(description='create a graph representing amino acid frequency in GISAID clade')
+parser.add_argument('protein_name', help='input protein name')
+parser.add_argument('aa_change', help='input amino acid change (e.g. R203K)')
+args = parser.parse_args()
 
-gisaid_file = glob.glob("path to the directory that have the files of amino acid frequencies")
-
-for clade_folder, clade_name in zip(gisaid_file, clade_names):
-    protein_file = glob.glob('{}/*'.format(clade_folder))
-    for graph_file in protein_file:
-        if protein_file[2] == graph_file or protein_file[9] == graph_file or protein_file[13] == graph_file or protein_file[18] == graph_file:
-            if protein_file[2] == graph_file: #if the file is N protein
-                protein_name = protein_names[0]
-            elif protein_file[9] == graph_file: #if the file is NS9c
-                protein_name = protein_names[1]
-            elif protein_file[13] == graph_file:
-                protein_name = protein_names[2]
-            elif protein_file[18] == graph_file: #if the file is NSP2
-                protein_name = protein_names[3]  
-
+#function to create data for graph
+def make_graph_data(protein):
+    gisaid_file_freq_posi = glob.glob("/mnt/c/Users/naoki/Onedrive/デスクトップ/MSc_research/clade_frequency/Frequency_results/clade_aa_frequency_correct/GISAID/**/*.*", recursive=False) #files must be csv files
+    for graph_file in gisaid_file_freq_posi:
+        graph_file_protein = re.split('[/_.]', graph_file)[-2] #The file name must be 'CladeName_ProteinName.csv'
+        graph_file_clade = re.split('[/_.]', graph_file)[-3]
+        if graph_file_protein == protein:
             df = pd.read_csv(graph_file)
 
             df2 = df.iloc[:, 1::2]
@@ -37,7 +34,65 @@ for clade_folder, clade_name in zip(gisaid_file, clade_names):
             df_fin = pd.DataFrame(lis)
             df_fin.insert(0, 'position', df_fin.index+1)
             df_fin.dropna(how ='all', axis=1, inplace=True) 
-            df_fin.to_csv("name and path of the csv file", index=False)
+            df_fin.to_csv("/mnt/c/Users/naoki/Onedrive/デスクトップ/github_source_code/{}_{}.csv".format(graph_file_clade, graph_file_protein), index=False)
 
+#function to create a graph
+def create_graph(protein, aa_change):
+    clade_names = []
+    position = int(aa_change[1:-1])
+    freq_list = []
+    gisaid_file_freq_aa = glob.glob("/mnt/c/Users/naoki/Onedrive/デスクトップ//github_source_code/*.csv") #files must be csv files
+    for graph_file in gisaid_file_freq_aa:
+        graph_file_protein = re.split('[/_.]', graph_file)[-2] #The file name must be 'CladeName_ProteinName.csv'
+        graph_file_clade = re.split('[/_.]', graph_file)[-3]
+        if graph_file_protein == protein:
+            clade_names.append(graph_file_clade)
+            df = pd.read_csv(graph_file)
+            df2 = df.drop(df.columns[0], axis=1)
+            df2 = df2.fillna(0).astype('int64', errors='ignore')
+            df2 = df2.astype(float)
+        #calculate the ratio of amino acids in the position
+            sum_count = 0
+            for m in range(len(df2.iloc[0])):
+                sum_count += df2.iloc[0][m]
+            for j in range(len(df2.index)):
+                for k in range(len(df2.iloc[j])):
+                    percentage = df2.iloc[j][k] / sum_count * 100
+                    df2.iloc[j][k] = percentage
+            freq_list.append(df2.iloc[position-1])
+        
+    #create a graph
+    df2 = pd.DataFrame(freq_list)
+    df2 = df2.reset_index()
+    df2 = df2.drop(df2.columns[0], axis=1)
+    columns_list = df2.columns.values
+
+    df2 = df2.values
+
+    plt.style.use('seaborn-bright')
+    length_f = df2.shape[1]
+
+    bottom = 0
+
+    fig, ax = plt.subplots()
+    color = ['lightgray','red','blue','green','yellow','purple', 'cyan', 'magenta','pink', 'orange', 'gray', 'rosybrown', 'olive', 'deepskyblue', 'navy', 'yellowgreen', 'teal', 'moccasin', 'lime', 'salmon', 'aquamarine']
+    for i in range(length_f):
+        ax.bar(clade_names, df2[:, i], bottom=bottom, align="center", color=color[i], width=0.5)
+        bottom += df2[:, i]
+
+    ax.set_xlabel('clade')
+    ax.set_ylabel('frequency')
+    ax.legend(columns_list, ncol=2, loc="upper left", bbox_to_anchor=(1.01, 1.01,), title='amino acid', fontsize=5.5).get_title().set_fontsize(6.5)
+    plt.title('{} {}'.format(protein, aa_change))
+
+    plt.show()
+
+    fig.savefig("/mnt/c/Users/naoki/Onedrive/デスクトップ/{}_{}.png".format(protein, aa_change), dpi=300, bbox_inches='tight')
+
+input_protein = args.protein_name
+input_aa_change = args.aa_change
+
+make_graph_data(input_protein)
+create_graph(input_protein, input_aa_change)
 
 # %%
