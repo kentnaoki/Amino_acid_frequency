c2866d6cda926bfe279bc3071b0e95f2a12730f9
kentnaoki <72868534+kentnaoki@users.noreply.github.com>
2021-09-19 16:20:49 +0100

    Add files via upload

--- Amino_acid_frequency/2_sequence_alignment.py
+++ Amino_acid_frequency/2_sequence_alignment.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import re
 
 def fasta2dict(seq):
 	#convert seqeuence to fasta
@@ -59,7 +60,7 @@ def clustalo(alnfile, outfile):
 #create directory to put files
 wd = "path to the directory"
 clade = ["L", "S", "V", "G", "GH", "GR", "GV", "O"]
-protein = ["E", "M", "N", "NS3", "NS6", "NS7a", "NS7b", "NS8", "NS9b", "NS9c", "NSP1", "NSP10", "NSP11", "NSP12", "NSP13", "NSP14", "NSP15", "NSP16", "NSP2", "NSP3", "NSP4", "NSP5", "NSP6", "NSP7", "NSP8", "NSP9", "Spike"]
+
 try:
     os.mkdir(wd)
     for clade_name in clade:
@@ -70,43 +71,44 @@ except:
     pass
 
 #align the sequence of FASTA file
-gisaid_file = glob.glob("path to the directory having directories for each clade")
-for clade_file, clade_name in zip(gisaid_file, clade):
-    protein_file = glob.glob('{}/*'.format(clade_file))
-    for in_file, out_file in zip(protein_file, protein):
-        clus = fasta2dict(in_file)
-        clus = human_only(clus)
-        sequence_dict = filter_seqs2(clus)
-        make_clastalo_in(sequence_dict, wd + "path to FASTA file")
+gisaid_file = glob.glob("path to the FASTA files")
+for in_file in gisaid_file:
+    
+    clade_name = re.split('[/_.]', in_file)[-3]
+    protein = re.split('[/_.]', in_file)[-2]
+    clus = fasta2dict(in_file)
+    clus = human_only(clus)
+    sequence_dict = filter_seqs2(clus)
+    make_clastalo_in(sequence_dict, wd + "path to FASTA file")
 
-        clustalo(wd + "path to FASTA file", wd + "path to FASTA file")
+    clustalo(wd + "path to FASTA file", wd + "path to FASTA file")
 
-        def aln2dict(namey):
-            # Convert alignment to dictionary 
-            aln_in = {}
-            with open(namey) as aln:
-                for line in aln:
-                    if line.startswith(">"):
-                        namey = line.rstrip()
-                        aln_in[namey] = []
-                    else:
-                        aln_in[namey].append(line.rstrip())
-            aln2 = {}
-            for k, v in aln_in.items():
-                aln2[k[:]] = "".join(v)
-            return aln2
+    def aln2dict(namey):
+        # Convert alignment to dictionary 
+        aln_in = {}
+        with open(namey) as aln:
+            for line in aln:
+                if line.startswith(">"):
+                    namey = line.rstrip()
+                    aln_in[namey] = []
+                else:
+                    aln_in[namey].append(line.rstrip())
+        aln2 = {}
+        for k, v in aln_in.items():
+            aln2[k[:]] = "".join(v)
+        return aln2
 
-        def repopulate(aln_in, sequence_dict):
-            # repopulate alignment with non-unique sequences 
-            newf = open(wd + clade_name + "/output/" + clade_name + "_" + out_file + ".aln", "w")
-            for k, v in aln_in.items():
-                for k2, v2 in sequence_dict.items():
-                    if k in v2:
-                        for x in v2:
-                            newf.write(x + "\n")
-                            newf.write(v + "\n")
-            newf.close()
+    def repopulate(aln_in, sequence_dict):
+        # repopulate alignment with non-unique sequences 
+        newf = open(wd + clade_name + "/output/" + clade_name + "_" + protein + ".aln", "w")
+        for k, v in aln_in.items():
+            for k2, v2 in sequence_dict.items():
+                if k in v2:
+                    for x in v2:
+                        newf.write(x + "\n")
+                        newf.write(v + "\n")
+        newf.close()
 
-        aln_in = aln2dict(wd + clade_name + "/clustalo_out/" + clade_name + "_" + out_file + "_clustalo" + ".fasta")
+    aln_in = aln2dict(wd + clade_name + "/clustalo_out/" + clade_name + "_" + protein + "_clustalo" + ".fasta")
 
-        repopulate(aln_in, sequence_dict)
+    repopulate(aln_in, sequence_dict)
